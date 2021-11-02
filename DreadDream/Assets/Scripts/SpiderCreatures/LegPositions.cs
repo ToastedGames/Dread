e1982dd2b47782da0af3c93c8f934f5c16d1b43a
Schnitzelreaper <schnitzelreaper@gmail.com>
2021-11-02 17:38:33 +0100

    Monster IK improvements

--- DreadDream/Assets/Scripts/SpiderCreatures/LegPositions.cs
+++ DreadDream/Assets/Scripts/SpiderCreatures/LegPositions.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class LegPositions : MonoBehaviour
 {
+    public float speed;
     public float legLength;
     public float maxStepDistance;
     [Space]
+    public Transform desiredPosition;
     public Transform[] legEnds;
     public Transform[] desiredLegPositions;
     public LayerMask obstacles;
 
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        
         for (int i = 0; i < legEnds.Length; i++)
         {
-            if(Vector3.Distance(legEnds[i].position, desiredLegPositions[i].position) > maxStepDistance)
-            {
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, (desiredLegPositions[i].position - transform.position).normalized, legLength, obstacles);
-                if (hit)
-                {
-                    legEnds[i].position = hit.point;
-                }
-                else
-                {
-                    legEnds[i].position = desiredLegPositions[i].position;
-                }
+            
+            if(Vector3.Distance(legEnds[i].position, desiredLegPositions[i].position) > maxStepDistance || Vector3.Distance(legEnds[i].position,transform.position) > legLength)
+            {              
+
+                legEnds[i].position = desiredLegPositions[i].position;
+
             }
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, (legEnds[i].position - transform.position).normalized, legLength, obstacles);
+            if(hit)
+            {
+                legEnds[i].position = hit.point;
+            }
+
+            
         }
+        rb.position = Vector3.Lerp(transform.position, desiredPosition.position, speed * Time.deltaTime);
+        
     }
 }

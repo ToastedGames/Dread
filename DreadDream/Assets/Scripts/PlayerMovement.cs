7f6a749a9ef558255c2bd1bb3ebc5840c75699c8
ToastedGames <moritz@von-basum.de>
2021-11-05 13:42:38 +0100

    Play Movement ein bisschen verbessert

--- DreadDream/Assets/Scripts/PlayerMovement.cs
+++ DreadDream/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private Camera cam;
     public float speed;
+
+    Vector2 movement;
 
 
     void Start()
@@ -21,11 +22,19 @@ public class PlayerMovement : MonoBehaviour
     {
         transform.up = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
 
-        Vector2 movement = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) movement += Vector2.up;
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        movement = new Vector2(xInput, yInput);
+        /*if (Input.GetKey(KeyCode.W)) movement += Vector2.up;
         if (Input.GetKey(KeyCode.S)) movement += Vector2.down;
         if (Input.GetKey(KeyCode.D)) movement += Vector2.right;
-        if (Input.GetKey(KeyCode.A)) movement += Vector2.left;
-        rb.MovePosition( movement.normalized * Time.deltaTime * speed + rb.position);
+        if (Input.GetKey(KeyCode.A)) movement += Vector2.left;*/
+        //rb.MovePosition(movement.normalized * Time.deltaTime * speed + rb.position);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(movement.normalized * Time.deltaTime * speed + rb.position);
     }
 }
